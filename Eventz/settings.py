0db318dcdf43867cbb74205aedf1be3e7746d0aa
Sasidhar Panthagada <sasidharpanthagada@gmail.com>
2021-10-06 08:43:22 +0530

    s3 policy update

--- Eventz/settings.py
+++ Eventz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hlzox(0w+z#2lme96$g0w$8nz#g+kn+glbw2w+-y7g@xd&v21e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['my-eventz.herokuapp.com','127.0.0.1','localhost','example.com' ]
 
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'events',
     'storages',
+    'events',
 ]
 
 MIDDLEWARE = [
@@ -127,19 +127,28 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = '/static/'
+
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR,'build','static'),
 ]
+STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
 
-if not DEBUG:
-    from Eventz.aws.conf import *
-else:
-    STATIC_URL = '/static/'
-    STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
+AWS_ACCESS_KEY_ID = 'AKIAWFNY7OE3QBQSXJSX'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'my-events-bucket'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
+DEFAULT_FILE_STORAGE = 'events.storage_backends.MediaStorage'
 
-    MEDIA_URL = '/images/'
-    MEDIA_ROOT = os.path.join(BASE_DIR,'static')
+
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static')
 
 
 
